daae88e576a6ae83649cab5399b62947619411b7
itsdansi <arajanacharya108@gmail.com>
2023-05-25 18:11:19 +0545

    feat: add product and category filter

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -3,21 +3,30 @@ import fs from "fs";
 
 export const getAllProducts = (req: Request, res: Response) => {
   try {
-    const data = fs.readFileSync("data2/NCH_catalogue.json", "utf8");
+    // const data = fs.readFileSync("data2/NCH_catalogue.json", "utf8");
+    const data = fs.readFileSync("data2/Nichino_Plantinum_250523.json", "utf8");
     // const data = fs.readFileSync("data/NCH_catalogue.json", "utf8");
     const productData = JSON.parse(data);
 
     // Get the filter condition from the query parameters
-    const filterPoints = parseInt(req.query.points as string, 10);
+    // const startPoint = parseInt(req.query.maxpoints as string, 10);
+    // const endPoint = parseInt(req.query.minPoints as string, 10);
 
-    // Filter the products based on the dynamic condition
-    const filteredProducts = productData.flatMap((element: any) => {
-      return element.subcategory.flatMap((data: any) => {
-        return data.products.filter((product: any) => product.Points >= filterPoints);
-      });
-    });
+    // const filteredProducts = productData.filter(
+    //   (product: any) => product.Points >= startPoint
+    // );
+    // console.log(filterProducts);
 
-    res.json(filteredProducts);
+    // // Filter the products based on the dynamic condition
+    // const filteredProducts = productData.flatMap((element: any) => {
+    //   return element.subcategory.flatMap((data: any) => {
+    //     return data.products.filter((product: any) => product.Points >= filterPoints);
+    //   });
+    // });
+
+    console.log(productData);
+    res.json(productData);
+    return true;
   } catch (error) {
     console.log(error);
     res.status(500).json({message: "Internal Server Error"});
@@ -26,24 +35,68 @@ export const getAllProducts = (req: Request, res: Response) => {
 
 export const getProductsBySubCategory = (req: Request, res: Response) => {
   try {
-    const data = fs.readFileSync("data2/NCH_catalogue.json", "utf8");
+    // const data = fs.readFileSync("data2/NCH_catalogue.json", "utf8");
+    const data = fs.readFileSync("data2/Nichino_Plantinum_250523.json", "utf8");
     const productData = JSON.parse(data);
 
     // Get the filter condition from the query parameters
     const filterSubCategory = req.query.subcategory;
 
-    // Filter the products based on the dynamic condition
-    const filteredProducts = productData.flatMap((element: any) => {
-      return element.subcategory.filter((data: any) => (data.name = filterSubCategory));
-    });
+    // // Filter the products based on the dynamic condition
+    // const filteredProducts = productData.flatMap((element: any) => {
+    //   return element.subcategory.filter((data: any) => (data.name = filterSubCategory));
+    // });
 
-    const products = filteredProducts.flatMap((element: any) => {
-      return element.products;
-    });
+    // const products = filteredProducts.flatMap((element: any) => {
+    //   return element.products;
+    // });
 
-    // console.log("result", filteredProducts);
-    // res.json(filteredProducts);
-    res.json(products);
+    // // console.log("result", filteredProducts);
+    // // res.json(filteredProducts);
+    // res.json(products);
+
+    // const test = productData.map((prod: any) => prod["Sub-Category"]);
+
+    const filteredProducts = productData.filter(
+      (element: any) => element["Sub-Category"] == filterSubCategory
+    );
+
+    // console.log(test);
+    // console.log(filteredProducts);
+    return res.send(filteredProducts);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({message: "Internal Server Error"});
+  }
+};
+
+export const getAllCategory = (req: Request, res: Response) => {
+  try {
+    const data = fs.readFileSync("data2/Nichino_Plantinum_250523.json", "utf8");
+    const productData = JSON.parse(data);
+
+    const categories = productData.map((prod: any) => prod["Sub-Category"]);
+    const uniqueCategories = [...new Set(categories)];
+
+    return res.send(uniqueCategories);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({message: "Internal Server Error"});
+  }
+};
+
+export const getSingleProduct = (req: Request, res: Response) => {
+  try {
+    const data = fs.readFileSync("data2/Nichino_Plantinum_250523.json", "utf8");
+    const productData = JSON.parse(data);
+
+    const serialNumber = req.params.id;
+
+    const filteredProducts = productData.filter(
+      (element: any) => element["Sr no"] == serialNumber
+    );
+
+    return res.send(filteredProducts);
   } catch (error) {
     console.log(error);
     res.status(500).json({message: "Internal Server Error"});
--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,11 +1,18 @@
 import express from "express";
 import {isAuth} from "../middleware/isAuth";
-import {getAllProducts, getProductsBySubCategory} from "../controller/product.controller";
+import {
+  getAllCategory,
+  getAllProducts,
+  getProductsBySubCategory,
+  getSingleProduct,
+} from "../controller/product.controller";
 import {validateGetProductFilterSchema} from "../validators/requests/product.requests";
 
 const router = express.Router();
 
 router.get("/", isAuth, validateGetProductFilterSchema, getAllProducts);
 router.get("/get-by-subcategory", getProductsBySubCategory);
+router.get("/get-all-categories", getAllCategory);
+router.get("/get-single-product/:id", getSingleProduct);
 
 export default router;
